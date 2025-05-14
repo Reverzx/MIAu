144133ba91a6507d74368c2ab2fca2f89121dff4
Mikhail <Rixed@yandex.ru>
2025-05-15 02:45:23 +0700

    commit

--- test/UI/test_delete_contact.py
+++ test/UI/test_delete_contact.py
@@ -1,60 +1,56 @@
-import pytest
 from loguru import logger
 from test_data.env import Env
-from pages.login_page import LoginPage
-from test_data.user_creds import UserCredentials
+from pages.add_contact_page import AddContactPage
+from test_data.user_creds import UserCredentials as UC
+from test_data.contacts_data import new_cont_valid_data as ncvd
 
 
-@pytest.mark.parametrize(
-    "email, password",
-    [
-        (UserCredentials.zm_email, UserCredentials.zm_password),
-    ],
-)
-def test_deleting_a_contact_positive_scenario(driver, email, password):
+def test_deleting_a_contact_positive_scenario(driver):
     """
     Verifies successful contact deletion with valid user credentials
     """
-    login_page = LoginPage(driver, Env.URL_Login)
-    login_page.open()
-
-    contact_list_page = login_page.complete_login(email, password)
-    assert contact_list_page.is_url_correct(Env.URL_ContactList), \
-        f"Expected URL: {Env.URL_ContactList}, but return: {driver.current_url}"
-    logger.info(
-        "The user is logged in"
+    # Open contact list page and add contact
+    addition = AddContactPage(driver, Env.URL_AddContact)
+    contact_list_page = addition.login_and_add_contact(
+        driver,
+        UC.usr_to_add_cont_email,
+        UC.usr_to_add_cont_password,
+        ncvd
     )
-    contact_details = contact_list_page.navigate_to_contact_details_page()
-    assert contact_details is not None, "Don't open contact detail page - contact list is empty"
-    contact_details.delete_contact()
-    assert contact_list_page.is_url_correct(Env.URL_ContactList), \
+    assert contact_list_page.is_url_correct(Env.URL_ContactList)
+
+    # Navigate to details contact
+    contact_details_page = contact_list_page.navigate_to_contact_details_page()
+    assert contact_details_page is not None, "Don't open contact detail page - contact list is empty"
+
+    # Delete contact and redirect to the contact list
+    contact_details_page.delete_contact()
+    assert contact_details_page.is_url_correct(Env.URL_ContactList), \
         f"Expected URL: {Env.URL_ContactList}, but return: {driver.current_url}"
     logger.success("The contact has been successfully deleted and the user "
                    "has been redirected to the contact list")
 
 
-@pytest.mark.parametrize(
-    "email, password",
-    [
-        (UserCredentials.zm_email, UserCredentials.zm_password),
-    ],
-)
-def test_deleting_a_contact_canceling(driver, email, password):
+def test_deleting_a_contact_canceling(driver):
     """
     Verifies that clicking "Нет" in the delete confirmation popup cancels deletion
     and user stays on the contact details or contact list page.
     """
-    login_page = LoginPage(driver, Env.URL_Login)
-    login_page.open()
-
-    contact_list_page = login_page.complete_login(email, password)
-    assert contact_list_page.is_url_correct(Env.URL_ContactList), \
-        f"Expected URL: {Env.URL_ContactList}, but return: {driver.current_url}"
-    logger.info(
-        "The user is logged in"
+    # Open contact list page and add contact
+    addition = AddContactPage(driver, Env.URL_AddContact)
+    contact_list_page = addition.login_and_add_contact(
+        driver,
+        UC.usr_to_add_cont_email,
+        UC.usr_to_add_cont_password,
+        ncvd
     )
-    contact_details = contact_list_page.navigate_to_contact_details_page()
-    assert contact_details is not None, "Don't open contact detail page - contact list is empty"
-    contact_details.cancel_delete_contact()
+    assert contact_list_page.is_url_correct(Env.URL_ContactList)
+
+    # Navigate to details contact
+    contact_details_page = contact_list_page.navigate_to_contact_details_page()
+    assert contact_details_page is not None, "Don't open contact detail page - contact list is empty"
+
+    # Try deleted contact and canceling delete
+    contact_details_page.cancel_delete_contact()
     assert contact_list_page.is_url_correct(Env.URL_ContactDetails), \
         f"Expected URL: {Env.URL_ContactDetails}, but return: {driver.current_url}"
